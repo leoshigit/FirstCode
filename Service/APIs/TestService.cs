46b6ce3da703b859481acd31962c9dc588cf2cbc
LeoShi <leoshimst@gmail.com>
2021-03-17 15:22:49 +0800

    修改

--- Service/APIs/TestService.cs
+++ Service/APIs/TestService.cs
@@ -13,13 +13,6 @@ namespace Service.APIs
 {
     public class TestService : ITestService
     {
-        //private string connectionStr;
-
-        //public TestService(IConfiguration configuration)
-        //{
-        //    this.connectionStr = configuration.GetConnectionString("dbName");
-        //}
-
         private IGenericRepository _genericRepository;
         public TestService(IGenericRepository genericRepository)
         {
@@ -28,13 +21,6 @@ namespace Service.APIs
 
         public List<TestViewModel> GetData()
         {
-            //using (SqlConnection conn = new SqlConnection(connectionStr))
-            //{
-            //    string sql = "select Id, Name from TempTable";
-            //    var result = conn.Query<TestViewModel>(sql).ToList();
-            //    return result;
-            //}
-
             string sql = "select Id, Name from TempTable";
             var result = _genericRepository.GetAll<TestViewModel>(sql);
             return result;
@@ -42,13 +28,6 @@ namespace Service.APIs
 
         public TestViewModel GetTestVM()
         {
-            //using (SqlConnection conn = new SqlConnection(connectionStr))
-            //{
-            //    string sql = "select Id, Name from TempTable";
-            //    var result = conn.Query<TestViewModel>(sql).FirstOrDefault();
-            //    return result;
-            //}
-
             string sql = "select Id, Name from TempTable";
             var result = _genericRepository.Get<TestViewModel>(sql);
             return result;
@@ -56,12 +35,6 @@ namespace Service.APIs
 
         public int Create(TestEditViewModel model)
         {
-            //using (SqlConnection conn = new SqlConnection(connectionStr))
-            //{
-            //    string sql = "insert into TempTable (Name) values (@Name)";
-            //    conn.Execute(sql, new { Name = model.Name });
-            //}
-
             string sql = "insert into TempTable (Name) values (@Name)";
             int id = _genericRepository.Create(sql, model);
             return id;
@@ -69,24 +42,12 @@ namespace Service.APIs
 
         public void Update(TestEditViewModel model)
         {
-            //using (SqlConnection conn = new SqlConnection(connectionStr))
-            //{
-            //    string sql = "update TempTable set Name = @Name where Id = @Id";
-            //    conn.Execute(sql, new { Id = model.Id, Name = model.Name });
-            //}
-
             string sql = "update TempTable set Name = @Name where Id = @Id";
             _genericRepository.Update(sql, model);
         }
 
         public void Delete(int id)
         {
-            //using (SqlConnection conn = new SqlConnection(connectionStr))
-            //{
-            //    string sql = "delete TempTable where Id = @Id";
-            //    conn.Execute(sql, new { Id = id });
-            //}
-
             string sql = "delete TempTable where Id = @Id";
             _genericRepository.Delete(sql, new { Id = id });
         }
